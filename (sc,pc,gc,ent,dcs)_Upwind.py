c4ecf2f4f3bdd89e68f97bb81b33a2fc85feb9d4
Julien1415 <108929045+Julien1415@users.noreply.github.com>
2024-07-16 14:56:18 +0200

    Update (sc,pc,gc,ent,dcs)_Upwind.py

--- (sc,pc,gc,ent,dcs)_Upwind.py
+++ (sc,pc,gc,ent,dcs)_Upwind.py
@@ -8,58 +8,99 @@ Created on Tue Jun  4 09:55:51 2024
 import numpy as np ; import matplotlib.pyplot as plt
 from time import time ; from time import sleep ; from datetime import timedelta
 
-def kappa(i):
-    if i==1:
-        return 0.0125
-    if i==2:
-        return 6
-    if i==3:
-        return 0.1
-    if i==4:
-        return 6
-    if i==5:
-        return 0.0375
+def kappa(k,head="None"):
+    if head == "None":
+        if k=="div,sc":
+            return 0.025
+        if k=="sc,pc":
+            return 0.025
+        if k=="div,pc":
+            return 0.02
+        if k=="pc,ent":
+            return 0.075
+        if k=="pc,gc":
+            return 0.075
+        if k=="ex,ent":
+            return 0.075
+        if k=="ex,gc":
+            return 0.075
+    elif head == "bar":
+        if k=="div,sc":
+            return 0.113
+        if k=="div,pc":
+            return 0.113
+        if k=="ex,ent":
+            return 0.113
+        if k=="ex,gc":
+            return 0.133
 
-def K(i):
-    if i==1:
-        return 0.06
-    if i==2:
-        return 53
-    if i==3:
-        return 0.275
-    if i==4:
-        return 41
-    if i==5:
-        return 0.95
+def K(k,head="None"):
+    if head == "None":
+        if k=="div,sc":
+            return 0.06
+        if k=="sc,pc":
+            return 0.06
+        if k=="div,pc":
+            return 0.2
+        if k=="pc,ent":
+            return 0.2
+        if k=="pc,gc":
+            return 0.2
+        if k=="ex,ent":
+            return 0.95
+        if k=="ex,gc":
+            return 0.95
+    elif head == "bar":
+        if k=="div,sc":
+            return 1
+        if k=="div,pc":
+            return 0.774
+        if k=="ex,ent":
+            return 0.377
+        if k=="ex,gc":
+            return 0.377
 
-def R(i,X):
+def R(k,X,head="None"): #R_tilde, R_bar_tilde
     R = []
     for x in X:
-        if x<=K(i)-kappa(i):
+        if x <= K(k,head)-kappa(k,head):
             R.append(0)
-        elif K(i)-kappa(i)<x<K(i)+kappa(i):
-            alpha = -x**3 + 3*K(i)*x**2 - (3*K(i)**2-3*kappa(i)**2)*x + K(i)**3+2*kappa(i)**3-3*K(i)*kappa(i)**2
-            R.append(alpha/(4*kappa(i)**3))
-        elif K(i)+kappa(i)<=x:
+        elif K(k,head)-kappa(k,head) < x < K(k,head)+kappa(k,head):
+            alpha = -x**3 + 3*K(k,head)*x**2 - (3*K(k,head)**2-3*kappa(k,head)**2)*x + K(k,head)**3+2*kappa(k,head)**3-3*K(k,head)*kappa(k,head)**2
+            R.append(alpha/(4*kappa(k,head)**3))
+        elif K(k,head)+kappa(k,head) <= x:
             R.append(1)
         else:
             print("Erreur",x)
     return np.array(R)
 
-def f1(x,utot):
-    q1 = 4822.152 ; q2 = 6429.536
-    return q1*(1-R(1,x))*(1-R(2,K(2)*(utot))) - q2*R(1,x)
+q_div_sc = 1112.109
+q_div_pc = 2718.488
+q_ex_ent = 4201.3
+q_ex_gc = 4201.3
+q_sc_pc = 2471.353
+q_pc_ent = 1853.515
+q_pc_gc = 611.66
 
-def f2(x,utot):
-    q3 = 7072.489 ; q4 = 10930.211
-    return q3*(1-R(3,x))*(1-R(4,K(2)*(utot))) - q4*R(5,x)*R(4,K(2)*(utot))
+def f_sc(x,rho_tot,rho_sc):
+    return rho_sc*q_div_sc*(1-R("div,sc",x))*(1-R("div,sc",rho_tot,"bar")) - rho_sc*q_sc_pc*R("sc,pc",x)
 
-q2 = 6429.536
+def f_ent(x,rho_tot,rho_pc,rho_ent):
+    return rho_pc*q_pc_gc*R("pc,gc",x) - rho_ent*q_ex_ent*R("ex,ent",x)*R("ex,ent",rho_tot,"bar")
+
+def f_gc(x,rho_tot,rho_pc,rho_gc):
+    return rho_pc*q_pc_gc*R("pc,gc",x) - rho_gc*q_ex_gc*R("ex,gc",x)*R("ex,gc",rho_tot,"bar")
+
+def f_pc(x,rho_tot,rho_pc,rho_sc):
+    return rho_pc*q_div_pc*(1-R("div,pc",x))*(1-R("div,pc",rho_tot,"bar")) - rho_pc*q_pc_ent*R("pc,ent",x) - rho_pc*q_pc_gc*R("pc,gc",x) + rho_sc*q_sc_pc*R("sc,pc",x)
 
 def phi(X):
-    r0 = 34
-    eps = 4
-    xmax = 400
+    # r0 = 34
+    # eps = 4
+    # xmax = 400
+    r0 = 25
+    eps = 2.5
+    xmax = 200
     phi = []
     for x in X:
         if x<=(r0-eps)/xmax :
@@ -75,15 +116,15 @@ def phi(X):
             print("Erreur phi")
     return np.array(phi)
 
-def f(p,pp,dx,phi_vect): #pp = ptot-p
-    y = - 1/dx**2*( np.maximum(p[1:-1]+pp[1:-1]-p[2:]-pp[2:],0)*p[1:-1]*phi_vect[1:-1] \
-                  + np.minimum(p[1:-1]+pp[1:-1]-p[2:]-pp[2:],0)*p[2:]*phi_vect[2:] \
-                  - np.maximum(p[:-2]+pp[:-2]-p[1:-1]-pp[1:-1],0)*p[:-2]*phi_vect[:-2] \
-                  - np.minimum(p[:-2]+pp[:-2]-p[1:-1]-pp[1:-1],0)*p[1:-1]*phi_vect[1:-1] )
+def f(rho,rho_almost_tot,dx,phi_vect): #scheme function; rho_almost_tot = rho_tot-rho
+    y = - 1/dx**2*( np.maximum(rho[1:-1]+rho_almost_tot[1:-1]-rho[2:]-rho_almost_tot[2:],0)*rho[1:-1]*phi_vect[1:-1] \
+                  + np.minimum(rho[1:-1]+rho_almost_tot[1:-1]-rho[2:]-rho_almost_tot[2:],0)*rho[2:]*phi_vect[2:] \
+                  - np.maximum(rho[:-2]+rho_almost_tot[:-2]-rho[1:-1]-rho_almost_tot[1:-1],0)*rho[:-2]*phi_vect[:-2] \
+                  - np.minimum(rho[:-2]+rho_almost_tot[:-2]-rho[1:-1]-rho_almost_tot[1:-1],0)*rho[1:-1]*phi_vect[1:-1] )
 
     return y
 
-def solve_crypt(sc_0,pc_0,dcs,gc_0,ent_0,nt,dt,dx,Xe,f1,f2,phi):
+def solve_crypt(sc_0,pc_0,dcs,gc_0,ent_0,nt,dt,dx,Xe,f_sc,f_pc,f_gc,f_ent,phi):
     tps = time()
 
     sc_mat = np.zeros((1,len(sc_0)))
@@ -102,7 +143,7 @@ def solve_crypt(sc_0,pc_0,dcs,gc_0,ent_0,nt,dt,dx,Xe,f1,f2,phi):
     ent_mat[0,:] = ent_0
     ent_old = ent_0
 
-    mem = 100 ; mem = min(nt,mem)
+    mem = 100 ; mem = min(nt,mem) #number of time step kept in memory
     list_t = [0]
 
     for n in range(1,nt):
@@ -112,25 +153,23 @@ def solve_crypt(sc_0,pc_0,dcs,gc_0,ent_0,nt,dt,dx,Xe,f1,f2,phi):
         gc_new = np.zeros_like(gc_old)
         ent_new = np.zeros_like(ent_old)
 
-        sc_new[1:-1] = sc_old[1:-1] + dt*f(sc_old,pc_old+dcs,dx,phi(Xe)) + dt*sc_old[1:-1]*f1(Xe[1:-1],sc_old[1:-1]+pc_old[1:-1]+dcs[1:-1])
-        pc_new[1:-1] = pc_old[1:-1] + dt*f(pc_old,sc_old+dcs,dx,phi(Xe)) + dt*sc_old[1:-1]*q2*R(1,Xe[1:-1]) + dt*pc_old[1:-1]*f2(Xe[1:-1]+sc_old[1:-1]+pc_old[1:-1],dcs[1:-1])
-        # gc_new[1:-1] = gc_old[1:-1] + dt*f(gc_old,sc_old+pc_old+dcs,dx,phi(Xe)) +
-        # ent_new[1:-1] = ent_old[1:-1] + dt*f(ent_old,sc_old+pc_old+dcs,dx,phi(Xe)) +
-
-        #f(...,...+gc+ent) #!!!
+        sc_new[1:-1] = sc_old[1:-1] + dt*f(sc_old,pc_old+gc_old+ent_old+dcs,dx,phi(Xe)) + dt*f_sc(Xe[1:-1],sc_old[1:-1]+pc_old[1:-1]+gc_old[1:-1]+ent_old[1:-1]+dcs[1:-1],sc_old[1:-1])
+        pc_new[1:-1] = pc_old[1:-1] + dt*f(pc_old,sc_old+gc_old+ent_old+dcs,dx,phi(Xe)) + dt*f_pc(Xe[1:-1],sc_old[1:-1]+pc_old[1:-1]+gc_old[1:-1]+ent_old[1:-1]+dcs[1:-1],pc_old[1:-1],sc_old[1:-1])
+        gc_new[1:-1] = gc_old[1:-1] + dt*f(gc_old,sc_old+pc_old+ent_old+dcs,dx,phi(Xe)) + dt*f_gc(Xe[1:-1],sc_old[1:-1]+pc_old[1:-1]+gc_old[1:-1]+ent_old[1:-1]+dcs[1:-1],pc_old[1:-1],gc_old[1:-1])
+        ent_new[1:-1] = ent_old[1:-1] + dt*f(ent_old,sc_old+pc_old+gc_old+dcs,dx,phi(Xe)) + dt*f_ent(Xe[1:-1],sc_old[1:-1]+pc_old[1:-1]+gc_old[1:-1]+ent_old[1:-1]+dcs[1:-1],pc_old[1:-1],ent_old[1:-1])
 
         sc_new[0] = sc_new[1] ; sc_new[-1] = sc_new[-2]
         pc_new[0] = pc_new[1] ; pc_new[-1] = pc_new[-2]
-        # gc_new[0] = gc_new[1] ; gc_new[-1] = gc_new[-2]
-        # ent_new[0] = ent_new[1] ; ent_new[-1] = ent_new[-2]
+        gc_new[0] = gc_new[1] ; gc_new[-1] = gc_new[-2]
+        ent_new[0] = ent_new[1] ; ent_new[-1] = ent_new[-2]
 
         sc_old = sc_new
         pc_old = pc_new
         gc_old = gc_new
         ent_old = ent_new
 
-        if (n%(nt//mem)==0) or (n==nt-1): #keep in memeory some of the iterations
-            print(f"\r{int(len(list_t)/mem*100)}% ",end="",flush=True) #print of the advancement in %
+        if (n%(nt//mem)==0) or (n==nt-1): #keep in memory some of the iterations
+            print(f"\r{min(int(len(list_t)/mem*100),100)}% ",end="",flush=True) #print of the advancement in %
             sleep(0.001)
             sc_mat = np.vstack([sc_mat,sc_new])
             pc_mat = np.vstack([pc_mat,pc_new])
@@ -142,12 +181,10 @@ def solve_crypt(sc_0,pc_0,dcs,gc_0,ent_0,nt,dt,dx,Xe,f1,f2,phi):
     return sc_mat,pc_mat,gc_mat,ent_mat,list_t
 
 def init_sc(x):
-    # return np.sqrt(np.maximum(0,1-64*(1/4-x)**2))
     # return np.sqrt(np.maximum(0,1-5000*(0.05-x)**2))
-    return np.sqrt(np.maximum(0,1-150*(0.0-x)**2))*0.35
+    return np.sqrt(np.maximum(0,1-300*(0.0-x)**2))*0.35
 
 def init_pc(x):
-    # return np.sqrt(np.maximum(0,1-64*(3/4-x)**2))*0.6
     return 0.0*x
 
 def init_gc(x):
@@ -157,88 +194,92 @@ def init_ent(x):
     return 0.0*x
 
 def init_dcs(X): #dcs as in the article
-    D = 8
+    # D = 8
+    # e = -0.06667
+    # d = 20
+    # N_dcs = 36
+    # xe = 6
+    # xd = 3
+    # xmax = 400
+    D = 12.3
     e = -0.06667
-    d = 20
-    N_dcs = 36
+    d = 2.25
+    N_dcs = 12
     xe = 6
     xd = 3
-    xmax = 400
-    toto = (2*D*e*d*N_dcs)/(K(2)*(2*e*d*(xe-xd)+e-d))
+    xmax = 200
+    # toto = (2*D*e*d*N_dcs)/(K("div,sc","bar")*(2*e*d*(xe-xd)+e-d))
+    toto = (2*D*e*d*N_dcs)/(K("div,sc","bar")*(2*e*d*(xe-xd)+e-d))*0.045
     dcs = []
-    decal = 0.1
+    shift = 0.1 #shift of dcs
     for x in X:
-        if (d*xd-1)/(d*xmax)<x-decal<xd/xmax :
-            dcs.append(toto*(1+d*(xmax*(x-decal)-xd)))
-        elif xd/xmax<=x-decal<=xe/xmax:
+        if (d*xd-1)/(d*xmax)<x-shift<xd/xmax :
+            dcs.append(toto*(1+d*(xmax*(x-shift)-xd)))
+        elif xd/xmax<=x-shift<=xe/xmax:
             dcs.append(toto)
-        elif xe/xmax<x-decal<(e*xe-1)/(e*xmax):
-            dcs.append(toto*(1+e*(xmax*(x-decal)-xe)))
+        elif xe/xmax<x-shift<(e*xe-1)/(e*xmax):
+            dcs.append(toto*(1+e*(xmax*(x-shift)-xe)))
         else :
             dcs.append(0)
     return np.array(dcs)
 
 #%% Mesh and initialisation
 
-long = 1.0 ; coef_cfl = 0.4 ; tmax = 0.01 ; dx = 1/250 #choose the values of the cfl, tmax and dx you want
+long = 1.0 ; coef_cfl = 0.4 ; tmax = 0.1 ; dx = 1/100 #choose the values of the cfl, tmax and dx you want
 dt = coef_cfl*dx**2 ; dt = 1/(int(1/dt)+1) #dt following the cfl and such as the inverse is an integer
-nx = round(long/dx-1) ; Xe,dx = np.linspace(0,long,nx+2,retstep=True)
-nt = round(tmax/dt+1) ; Xt,dt = np.linspace(0,tmax,nt,retstep=True)
+nx = round(long/dx-1) ; Xe,dx = np.linspace(0,long,nx+2,retstep=True) #mesh in space
+nt = round(tmax/dt+1) ; Xt,dt = np.linspace(0,tmax,nt,retstep=True) #mesh in time
 
 sc_0 = init_sc(Xe) ; pc_0 = init_pc(Xe) ; dcs = init_dcs(Xe) ; gc_0 = init_gc(Xe) ; ent_0 = init_ent(Xe) #initialisation
 
 cfl = dt/dx**2 ; print(f"{nx+1} mesh cells, {nt-1} iterations in time, dt/dx**2 = {np.round(cfl,4)}")
 
 #Solve
-sc_mat,pc_mat,gc_mat,ent_mat,t = solve_crypt(sc_0,pc_0,dcs,gc_0,ent_0,nt,dt,dx,Xe,f1,f2,phi) #solve
-
-#%% Graphs source terms, phi
-
-# plt.figure()
-# plt.plot(Xe,f1(Xe,300+dcs),'-',label="$f_1$")
-# plt.plot(Xe,f2(Xe,300+dcs),'-',label="$f_2$")
-# plt.legend()
-
-# plt.figure()
-# plt.plot(Xe,R(1,Xe),label="R1")
-# # plt.plot(Xe*K(2),R(2,Xe*K(2)),label="R2")
-# plt.plot(Xe,R(3,Xe),label="R3")
-# # plt.plot(Xe*K(2),R(4,Xe*K(2)),label="R4")
-# plt.plot(Xe,R(5,Xe),label="R5")
-# plt.legend()
-
-# plt.figure()
-# plt.plot(Xe,phi(Xe),label="$\phi$")
-# plt.legend()
+sc_mat,pc_mat,gc_mat,ent_mat,t = solve_crypt(sc_0,pc_0,dcs,gc_0,ent_0,nt,dt,dx,Xe,f_sc,f_pc,f_gc,f_ent,phi)
 
 #%% Plot
 
-# Initial conditions sc_0,pc_0,dcs
-# plt.figure() ; plt.plot(Xe,sc_0,'b--',label=r"$\rho_{sc}^0$") ; plt.plot(Xe,pc_0,'r--',label=r"$\rho_{pc}^0$") ; plt.plot(Xe,dcs,'m-',label=r"$\rho_{dcs}$") ;plt.xlim(0,1.1) ; plt.ylim(0,1.1) ; plt.xlabel("x") ; plt.legend() ; plt.title("Conditions initiales "+r"$\rho_{sc}^0,\rho_{pc}^0,\rho_{dcs},$"+f" N={nx+1}")
+# Initial conditions sc_0,pc_0,dcs ; graph phi
+# plt.figure() ; plt.plot(Xe,sc_0,'b--',label=r"$\rho_{sc}^0$") ; plt.plot(Xe,pc_0,'r--',label=r"$\rho_{pc}^0$") ; plt.plot(Xe,dcs,'m-',label=r"$\rho_{dcs}$") ;plt.xlim(0,1.1) ; plt.ylim(0,1.3) ; plt.xlabel("x") ; plt.legend() ; plt.title("Conditions initiales "+r"$\rho_{sc}^0,\rho_{pc}^0,\rho_{dcs},$"+f" N={nx+1}")
+# plt.figure() ; plt.plot(Xe,phi(Xe),label="$\phi$") ; plt.legend()
 
-mass_sc = dx*np.sum(sc_0) ; mass_pc = dx*np.sum(pc_0) ; mass_dcs = dx*np.sum(dcs) ; xshock = mass_sc/(mass_sc+mass_pc)
-
-def plot(sc_mat,pc_mat,dcs,sc_0,pc_0,t):
+def plot(sc_mat,pc_mat,gc_mat,ent_mat,dcs,sc_0,pc_0,t):
     plt.figure()
-    for n in range(np.shape(sc_mat)[0]):
+    for n in range(len(t)):
         plt.cla()
 
         plt.plot(Xe,sc_mat[n,:],'b.-',label=r"$\rho_{sc}$")
         # plt.plot(Xe,sc_mat[n,:]+dcs,'b.-',label=r"$\rho_{sc}(+\rho_{dcs})$")
-        plt.plot(Xe,sc_0,'b--',label=r"$\rho_{sc}(t=0)$")
+        if np.max(sc_0)>0:
+            plt.plot(Xe,sc_0,'b--',label=r"$\rho_{sc}(t=0)$")
+
         # plt.plot(Xe,pc_mat[n,:],'r.-',label=r"$\rho_{pc}$")
         plt.plot(Xe,pc_mat[n,:]+dcs,'r.-',label=r"$\rho_{pc}(+\rho_{dcs})$")
-        plt.plot(Xe,pc_0,'r--',label=r"$\rho_{pc}(t=0)$")
-        plt.plot(Xe,dcs,'m-',label=r"$\rho_{dcs}$")
-        plt.plot(Xe,sc_mat[n,:]+pc_mat[n,:]+dcs,'k--',label="total")
+        if np.max(pc_0)>0:
+            plt.plot(Xe,pc_0,'r--',label=r"$\rho_{pc}(t=0)$")
 
-        plt.ylim(-0.03,1.4)
+        # plt.plot(Xe,gc_mat[n,:],'.-',label=r"$\rho_{gc}$")
+        plt.plot(Xe,gc_mat[n,:]+dcs,'.-',label=r"$\rho_{gc}(+\rho_{dcs})$")
+        if np.max(gc_0)>0:
+            plt.plot(Xe,gc_0,'--',label=r"$\rho_{gc}(t=0)$")
+
+        # plt.plot(Xe,ent_mat[n,:],'.-',label=r"$\rho_{ent}$")
+        plt.plot(Xe,ent_mat[n,:]+dcs,'.-',label=r"$\rho_{ent}(+\rho_{dcs})$")
+        # plt.plot(Xe,ent_mat[n,:]+gc_mat[n,:],'.-',label=r"$\rho_{ent}(+\rho_{gc})$")
+        plt.plot(Xe,ent_mat[n,:]+gc_mat[n,:]+dcs,'.-',label=r"$\rho_{ent}+\rho_{gc}(+\rho_{dcs})$")
+        if np.max(ent_0)>0:
+            plt.plot(Xe,ent_0,'--',label=r"$\rho_{ent}(t=0)$")
+
+        plt.plot(Xe,dcs,'m-',label=r"$\rho_{dcs}$")
+        plt.plot(Xe,sc_mat[n,:]+pc_mat[n,:]+gc_mat[n,:]+ent_mat[n,:]+dcs,'k--',label="total")
+
+        # plt.ylim(-0.03,1.3)
+        plt.ylim(0.0,1.3)
         plt.xlim(0,0.99)
         plt.legend()
         plt.title(f"Upwind, N={nx+1}, t={round(t[n]*dt,4)}")
-        plt.xlabel(f"dt/dx**2={np.round(cfl,3)}, mass(u0)={np.round(mass_sc+mass_pc,4)}")
+        plt.xlabel(f"dt/dx**2={np.round(cfl,3)}")
         plt.ylabel("Densities")
         plt.pause(0.01+0.5/(n**2+1))
 
-plot(sc_mat,pc_mat,dcs,sc_0,pc_0,t)
-
+plot(sc_mat,pc_mat,gc_mat,ent_mat,dcs,sc_0,pc_0,t)
